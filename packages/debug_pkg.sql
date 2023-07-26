cb794e9e65a0372782cdd8192f2b58a62be58334
reetdipti <reetdipti@gmail.com>
2023-07-25 19:38:50 -0500

    updated debug_memo to save timestamp

--- packages/debug_pkg.sql
+++ packages/debug_pkg.sql
@@ -1,1 +1,26 @@
-select * from family
+CREATE OR REPLACE package debug_pkg 
+is
+    procedure p_debug_memo(i_memo in clob, i_mode in varchar2 default null);
+end debug_pkg;
+/
+
+
+CREATE OR REPLACE package body debug_pkg
+as
+    -- procedure to insert memo
+    procedure p_debug_memo(i_memo in clob, i_mode in varchar2 default null)
+    is
+    begin   
+
+        if(i_mode = 'delete') then
+            delete from debug_memo;
+        end if;
+
+        insert into debug_memo(memo, time_stamp)
+        values (i_memo, sysdate);
+
+        commit;
+
+    end p_debug_memo;
+end debug_pkg;
+/
