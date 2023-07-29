6939c4d08db714a52195e8ffa2708f706fee2c9a
reetdipti <reetdipti@gmail.com>
2023-07-29 17:24:45 -0500

    test

---
+++ procedures/sp_test_procedure.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE PROCEDURE sp_test_procedure
+IS
+    l_name VARCHAR2(120);
+BEGIN
+    
+    l_name := 'rert';
+    
+    dbms_output.put_line('Name: ' || l_name);--hjhj
+END sp_test_procedure;
---
+++ scripts/family_helps.sql
@@ -0,0 +1,4 @@
+select * from family;
+
+begin debug_pkg.p_debug_memo('test'); end;
+select * from debug_memo;   
